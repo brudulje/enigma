f6272933afaf8bd50c0ce832b7552d48349949a3
Joachim Seland Graff <Joachim.Seland.Graff@gmail.com>
2021-02-21 19:16:38 +0100

    getWalzen has been written.

--- schlusselMacher.py
+++ schlusselMacher.py
@@ -5,6 +5,9 @@ Created on Sun Feb 21 16:27:47 2021
 @author: jmg
 """
 
+import datetime
+import secrets
+
 """
 Generate daykeys for the Enigma_M3.
 
@@ -23,13 +26,11 @@ out which key applies to a message. There are 26**3 = 17576 different
 kenngruppen. With 4 kenngruppen each day, this is sufficient for 12 years
 before a kenngruppen must be used again. Thus, in this key generator, the
 kenngruppen are not reused.
+
+Keys are generated for one month at a time. It is assumed that keys are
+generated for the month following the current month.
 """
 
-import datetime
-import secrets
-
-
-# Keys are generated for one month at a time.
 
 def main():
 
@@ -39,19 +40,19 @@ def main():
     outfile = "enigmaSchlussel" + month + ".txt"
 
     with open(outfile, "w") as out:
-        # For simplicity, each month will have 31 daykeys.
 
         # Write header
         out.write("GEHEIM!")
-        out.write(f"{'brudulje enigmaSchlussel' : >48}")
+        out.write(f"{'brudulje enigmaSchlussel' : >49}")
         monthString = nextMonth.strftime("%B %Y")
-        out.write(f"{monthString : >33} \n")
+        out.write(f"{monthString : >32} \n")
         out.write(89 * "-" + "\n")
-        out.write("|Tag |  Walzenlage    | Ringstellung |      "
+        out.write("|Tag |   Walzenlage   | Ringstellung |      "
                   + "Steckerverbindungen       |   Kenngruppen   |\n")
         out.write(89 * "-" + "\n")
 
         # Get, format and write daykey for each day
+        # For simplicity, each month will have 31 daykeys.
         for day in range(31, 0, -1):
             walzen = getWalzen()
             rings = getRings()
@@ -72,7 +73,18 @@ def main():
 
 def getWalzen():
     # TODO: Make proper function.
-    return "I", "II", "III"
+    walzen = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
+    # print(walzen)
+    w1 = secrets.choice(walzen)  # Pick one at random
+    walzen.pop(walzen.index(w1))  # Remove it from the pool
+    # print(walzen)
+    w2 = secrets.choice(walzen)
+    walzen.pop(walzen.index(w2))
+    # print(walzen)
+    w3 = secrets.choice(walzen)
+    walzen.pop(walzen.index(w3))
+    # print(walzen)
+    return w1, w2, w3
 
 
 def getRings():
