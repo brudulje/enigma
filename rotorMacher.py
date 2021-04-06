3a767155f6a5aa03f20e13c8773612283d60b825
Joachim Seland Graff <Joachim.Seland.Graff@gmail.com>
2021-04-06 21:21:37 +0200

    Wrote generator for reflectors.
    The general reflector generator does not work, but the atbash
    version does. Settle for atbash for now to see that thins work.
    
    Wrote rotor generator as a function.

--- rotorMacher.py
+++ rotorMacher.py
@@ -7,24 +7,64 @@ Created on Mon Apr  5 19:12:29 2021
 import random
 import roman
 
-alphabet = list("!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÁÉÍÏÐÓÞÆÄØÖÅáéíïðóþæäøöåßΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψωАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя")
+alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÁÉÍÏÐÓÞÆÄØÖÅáéíïðóþæäøöåΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψωАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяᚠᚢᚦᚨᚬᚱᚲᚴᚷᚹᚺᚼᚾᛁᛃᛅᛇᛈᛉᛊᛋᛏᛒᛖᛗᛘᛚᛜᛞᛟᛦᛮᛯᛰ"
 
+alphabet = list(alphabet)
 # for n in range(1, 12):
 #     print(roman.toRoman(n))
 
 # Rotor {RomanNumeral:
 #           ["NameRoman", "Scrambled alphabet", ["List", "of", "notches"]}
 
-for n in range(1,4):
 
-    nRoman = roman.toRoman(n)
+def genereate_rotors(alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                     start=1,
+                     stop=4,
+                     notch_percentage=0.316227766):
+    """Return dict of valid Enigma style rotors."""
+    rotors = {}
+    for n in range(start, stop):
+        nRoman = roman.toRoman(n)
 
-    scrambled = random.sample(alphabet, len(alphabet))
-    scrambled = "".join(scrambled)
-    # print(scrambled, {len(scrambled)})
+        scrambled = random.sample(alphabet, len(alphabet))
+        scrambled = "".join(scrambled)
+        # print(scrambled, {len(scrambled)})
 
-    notches = [i for i in alphabet if random.random() < 0.316227766]
-    # print(notches, {len(notches)})
+        notches = [i for i in alphabet if random.random() < notch_percentage]
+        # print(notches, {len(notches)})
 
-    rotor = [nRoman, scrambled, notches]
-    print(rotor)
+        rotor = [nRoman, scrambled, notches]
+        # print(rotor)
+        rotors[nRoman] = rotor
+
+        return rotors
+
+
+def generate_reflector(alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+    alphabet = list(alphabet)
+    nums = list(range(len(alphabet)))
+    while len(nums) != 0:
+        n = random.randrange(len(nums))
+        m = random.randrange(len(nums))
+        while n == m:
+            # Make sure n != m
+            n = random.randrange(len(nums))
+            m = random.randrange(len(nums))
+        # Swap elements
+        alphabet[n], alphabet[m] = alphabet[m], alphabet[n]
+        # Remove elements which are swapped
+        _ = nums.pop(n)
+        _ = nums.pop(m)
+    return alphabet
+
+
+def generate_atbash_reflector(alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+    # alphabet = list(alphabet)
+    # for n in range(len(alphabet)//2):
+    #     alphabet[n], alphabet[len(alphabet) - n - 1] \
+    #     = alphabet[len(alphabet) - n - 1], alphabet[n]
+    # alphabet = "".join(alphabet)
+    # return alphabet
+    return "".join(alphabet[::-1])
+
+print(generate_atbash_reflector(alphabet=alphabet))
